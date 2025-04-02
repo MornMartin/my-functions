faccbff3077dd1414ab705d7b3bbbb8c5fa34dfe
Fu Yan <beingwrong@live.com>
2025-04-02 14:56:59 +0800

    Create git_statistics.sh

---
+++ sh/git_statistics.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+#根目录
+rootpath=$(pwd)
+#源码目录
+source_codes=$rootpath
+#统计时段
+git_log_since="${1:-'2025-01-01'}"
+git_log_until="${2:-'2025-03-31'}"
+
+echo git_log_since：$git_log_since, git_log_until：$git_log_until
+
+# GIT用户列表
+declare -A user_map
+user_map["xxx"]=xxx
+user_map["xxx1"]=xxx
+
+# 代码统计列表
+declare -A line_map
+
+function get_log(){
+  git_log=$(git log --all --since =$git_log_since --until==$git_log_until --author=$1 --pretty=short)
+  echo $git_log
+}
+function get_commit_msg(){
+  git_commit=$(git show $1 --stat|grep insertion)
+  echo "$git_commit"
+}
+function get_note(){
+  cut_str=$1
+  git_note=$(git show $1 --stat --oneline |grep ${cut_str:0:7})
+  echo "$git_note"
+}
+function get_branch_name(){
+  echo "$(git branch -r --contains $1)"
+}
+
+function export_data() {
+	path=$1;
+    echo "进入目录：" $source_codes/$path;
+    cd $source_codes/$path;
+    
+    authors=$(git log --all --format="%aN" | sort -u)
+    IFS=' ' read -ra author_array <<< "`echo $authors`"
+    for author in "${author_array[@]}"; do
+      if [[ -n $(echo $author |grep @) ]] || [[ -z ${user_map[$author]} ]]; then
+        echo "未识别用户："$author""
+      else
+        git_log=$(get_log $author)
+        if [ -n "$git_log" ]; then
+          IFS=' ' read -ra array <<< "$git_log"
+          status=0
+          for line in "${array[@]}"; do
+            if [[ "$line" == "commit" ]];then
+              status=1
+            else
+              if [[ "$status" == "1" ]];then
+                if [ -z ${line_map[$line]} ]; then
+                  line_map["$line"]=1
+                else
+                  echo "重复数据：" $line
+                  status=0
+                  continue
+                fi
+                commit=$(get_commit_msg $line )
+                note=$(get_note $line )
+                branch_name=$(get_branch_name $line )
+                IFS=',' read -ra commit_array <<< "$commit"
+                for sub in "${commit_array[@]}"; do 
+                  if [[ "$sub" == *"+"* ]];then
+                    add_num=$(echo $sub|awk -F" " '{print $1}')
+                    echo "$add_num!$author!${user_map[$author]}!$line!$commit!$note!$path" >> $rootpath/logs
+                  fi
+                done
+              fi
+              status=0
+            fi
+          done
+        fi
+      fi
+    done
+}
+rm -rf $rootpath/logs
+touch $rootpath/logs
+for dir in $(ls $source_codes); do
+    export_data $dir;
+done
